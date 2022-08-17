0939108b8e36b8d8227ffaab4ceb4a4ed5937c5d
Didier Malenfant <coding@malenfant.net>
2022-08-17 21:41:12 +0200

    Fixed incorrect macro names

--- pdbase/pdbase.c
+++ pdbase/pdbase.c
@@ -21,7 +21,7 @@ void* pd_calloc(size_t nb_of_items, size_t item_size)
     }
 
     size_t size = nb_of_items * item_size;
-    void* memory = PD_ALLOC(size);
+    void* memory = PDBASE_ALLOC(size);
     if(memory != NULL) {
         memset(memory, 0, size);
     }
